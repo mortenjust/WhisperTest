90771f1ecd4a6b1734dd2b21667fdba50a6e497b
Morten Just <morten.just@genstart.dk>
2022-11-24 16:45:53 +0100

    Initial Commit

---
+++ WhisperTest/WhisperTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperTestApp.swift
+//  WhisperTest
+//
+//  Created by Morten Just on 11/24/22.
+//
+
+import SwiftUI
+
+@main
+struct WhisperTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
